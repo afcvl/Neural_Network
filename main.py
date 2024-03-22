c9aa5eda68606b87cf080f709d7d73e4840f354f
afcvl <afcarvalho@usp.br>
2024-03-21 22:09:48 -0300

    foward funcion

--- main.py
+++ main.py
@@ -32,30 +32,40 @@ class Perceptron():
             self.weights.append(random.uniform(-1,1))
     
 class MLP():
-    def __init__(self, n_inputs, n_exits, n_hiden_layer):
+    def __init__(self, n_inputs, n_hiden_layer, n_exits, activation='relu'):
         self.inputs = []
         self.exits = []
         self.hidden = []
         
+        self.network = [self.inputs, self.hidden, self.exits]
+        
         for i in range(n_inputs):
-            self.inputs.append(Perceptron())
+            self.inputs.append(Perceptron(activation))
             
         for i in range(n_hiden_layer):
-            self.hidden.append(Perceptron())
+            self.hidden.append(Perceptron(activation))
             self.hidden[i].init_weights(n_inputs)
         
         for i in range(n_exits):
-            self.exits.append(Perceptron())
+            self.exits.append(Perceptron(activation))
             self.exits[i].init_weights(n_hiden_layer)
             
-    def foward(self):
-        pass
+    def foward(self, input_data):
+        output_layer1 = input_data
+        output_layer2 = []
+        output_layer3 = []
+        
+        for neuron in self.hidden:
+            output_layer2.append(neuron.process_input(output_layer1))
             
+        for neuron in self.exits:
+            output_layer3.append(neuron.process_input(output_layer2))
             
-neuron = Perceptron('relu')
+        return output_layer3
+            
 
 values = [5,3,4,2,8,6]
 
-neuron.init_weights(len(values))
+nn = MLP(6,3,5)
 
-print(neuron.process_input(values))
+print(nn.foward(values))
