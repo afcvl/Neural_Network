4f67268f8b2d55ad3389e683f09bfadb18f5b102
afcvl <afcarvalho@usp.br>
2024-04-12 19:47:42 -0300

    refatoração do código

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import numpy as np
 from Activations import Activation
 from numpy import ndarray
 import pickle
-
+import time
 
 class Perceptron:
     def __init__(self, actvation, n_weights, weights=None):
-        self.n_weights = n_weights + 1
+        self.n_weights = n_weights
         self.count = 0
 
         if weights is None:
-            self.weights = (2 * np.random.rand(n_weights + 1).astype(np.float64)) - 1
+            self.weights = (2 * np.random.rand(n_weights).astype(np.float64)) - 1
             # gera valores de parametros entre -1 e 1
         else:
             self.weights = weights
@@ -36,51 +36,48 @@ class Perceptron:
 
         return self.activation(soma)
 
-
 class MLP:
-    def __init__(self, file_name='key.bitcoin'):
-        self.file_name = file_name
+    def __init__(self, layers_size):
         self.network = []
-        self.n_weights = 0
+
+        for i in range(1, len(layers_size)):
+            self.add_layer(n_inputs=layers_size[i-1]+1, 
+                           number_neurons=layers_size[i],
+                           activation='relu')
 
     def foward(self, input_data):
         input_values = input_data.copy()  # Cria uma cópia da lista original
         input_values.append(1)
         input_values = np.array(input_values, dtype=np.float64)
-        output_values = []
 
-        for layer in self.network[1:]:  # itera sobre as camadas excluindo a de entrada
+        for layer in self.network:  # itera sobre as camadas excluindo a de entrada
+            print(f'input: {input_values}')
+            layer_output_values = []
 
             for neuron in layer:
-                output_values.append(neuron.process_input(input_values))
-            output_values.append(1)  # append bias
-
+                layer_output_values.append(neuron.process_input(input_values))
+            
             # seta input_values que será usado na proxima camada
-            input_values = np.array(output_values, dtype=np.float64)
-            output_values = []
+            layer_output_values.append(1) # inserindo bias para proxima camada
+            print(f'Saída: {layer_output_values}')
+            input_values = np.array(layer_output_values, dtype=np.float64)
+            print()
+        return layer_output_values
 
-        output_values = np.delete(input_values, -1)
-
-        return output_values
-
-    def add(self, number_neurons, activation):
+    def add_layer(self, n_inputs, number_neurons, activation):
         neurons_list = []
 
         for _ in range(number_neurons):
-            neurons_list.append(Perceptron(activation, self.n_weights))
+            neurons_list.append(Perceptron(activation, n_inputs))
 
         self.network.append(neurons_list)
-        self.n_weights = number_neurons
 
-    def remove_last(self):
-        self.network.pop()
-
-    def salvar(self):
-        with open(self.file_name, 'wb') as f:
+    def save_network(self, file_name):
+        with open(file_name, 'wb') as f:
             pickle.dump(self.network, f)
 
-    def carregar(self):
-        with open(self.file_name, 'rb') as f:
+    def load_network(self, file_name):
+        with open(file_name, 'rb') as f:
             self.network = pickle.load(f)
 
     def treinar(self, epochs, training_data, validation_data=None, learning_rate=1, parada_cedo=-1):
@@ -108,20 +105,23 @@ class MLP:
                 else:
                     parada_cedo -= 1
 
-
 if __name__ == '__main__':
+    
     values = [1, 2, 3, 4, 5]
-    layers = [len(values), 5000, 2000, 100, 10]
-    nn = MLP()
-    for layer in layers:
-        nn.add(layer, 'relu')
-    nn.add(50, 'linear')
-    nn.remove_last()
-    nn.salvar()
-    saida1 = nn.foward(values)
-    # print("saida1:", saida1)
-    nn = MLP()
-    nn.carregar()
-    saida2 = nn.foward(values)
-    # print("saida2:", saida2)
-    print("diferença:", saida2-saida1)
+    
+    layers_size = [len(values), 5,3]
+
+    nn = MLP(layers_size)
+    
+    start_time = time.time()
+    for _ in range(1):
+        output = nn.foward(values)
+    end_time = time.time()
+    
+    avarage_time = (end_time-start_time)/1
+    
+    print()
+    print(f"tempo medio {avarage_time} s")
+    print(f'Saida da rede: {output}')
+
+    
